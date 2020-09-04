4dcabd97146f024e30b9fd80310c32f5f7e89943
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-04 22:28:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,9 @@ if(array_key_exists('button1', $_POST)) {
 function button1(){
   ignore_user_abort(true);
   require 'vendor/autoload.php';
-  $client = \Symfony\Component\Panther\Client::createChromeClient();
+  $client = \Symfony\Component\Panther\Client::createChromeClient(null, [
+    '--headfull'
+  ]);
   sleep(1);
   $crawler = $client->request('GET', 'https://www.youtube.com/results?search_query=php+web+scraping');
   sleep(3);
