731476e9f921db760134c43ac8a9510c59849fcd
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-04 21:02:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -22,11 +22,12 @@ function button1(){
   $client = \Symfony\Component\Panther\Client::createChromeClient(null, [
     '--headfull'
   ]);
-sleep(1);
-$crawler = $client->request('GET', 'https://www.youtube.com/results?search_query=php+web+scraping');
-sleep(3);
-$link = $crawler->selectLink('PHP Web Scraping - Simple HTML DOM Parser')->link();
-$crawler = $client->click($link);}
+  sleep(1);
+  $crawler = $client->request('GET', 'https://www.youtube.com/results?search_query=php+web+scraping');
+  sleep(3);
+  $link = $crawler->selectLink('PHP Web Scraping - Simple HTML DOM Parser')->link();
+  $crawler = $client->click($link);
+}
 button1();
 ?>
 </body>
