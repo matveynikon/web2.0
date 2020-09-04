8de0bb6238ec828da1f57090ca30de2919f525af
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-04 20:58:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,17 @@ if(array_key_exists('button1', $_POST)) {
 } 
 function button1(){
   ignore_user_abort(true);
-}
+  require 'vendor/autoload.php';
+  $client = \Symfony\Component\Panther\Client::createChromeClient(null, [
+    '--headfull'
+  ]);
+sleep(1);
+$crawler = $client->request('GET', 'https://www.youtube.com/results?search_query=php+web+scraping');
+sleep(15);
+
+$client->takeScreenshot('shot2.png');   
+$link = $crawler->selectLink('PHP Web Scraping - Simple HTML DOM Parser')->link();
+$crawler = $client->click($link);}
 button1();
 ?>
 </body>
