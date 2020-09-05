78bfe29df4ecbf8257fe4bd7e99297ed5ff716f2
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-05 12:15:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,26 +8,12 @@
 <?php
 if(array_key_exists('button1', $_POST)) { 
   button1(); 
-}
+} 
 function button1(){
   ignore_user_abort(true);
-  require 'vendor/autoload.php';
-  $client = \Symfony\Component\Panther\Client::createChromeClient(null, [
-      '--headfull'
-  ]);
-  //use Goutte\Client;
-  //$client = new Client();
-  sleep(1);
-  $crawler = $client->request('GET', 'https://www.youtube.com/results?search_query=php+web+scraping');
-  sleep(3);
-  $link = $crawler->selectLink('PHP Web Scraping - Simple HTML DOM Parser')->link();
-  $crawler = $client->click($link);
-  //echo($link);
+  exec("php goutte.php &");
 }
+button1();
 ?>
-<form method="post"> 
-   <input type="submit" name="button1"
-       class="button" value="Button1" /> 
-</form> 
 </body>
 </head>
