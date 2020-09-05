4bb4cf3a909ce97e327fb30505ac43fc379095e2
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-05 10:20:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-function button1(){
-  ignore_user_abort(true);
-  require 'vendor/autoload.php';
-  $client = \Symfony\Component\Panther\Client::createChromeClient(null, [
-    '--headfull'
-  ]);
-  sleep(1);
-  $crawler = $client->request('GET', 'https://www.youtube.com/results?search_query=php+web+scraping');
-  sleep(3);
-  $link = $crawler->selectLink('PHP Web Scraping - Simple HTML DOM Parser')->link();
-  $crawler = $client->click($link);
-}
-button1();
+echo("hey");
 ?>
