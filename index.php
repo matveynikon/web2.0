f16161711455e1ab22eb9cec8fe505284410bb10
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-09 16:52:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 <?php
   ignore_user_abort(true);  
   require 'vendor/autoload.php';
-  $client = /vendor/symfony/panther/Client::createChromeClient();
+  $client = \Symfony\Component\Panther\Client::createChromeClient();
   //use Goutte\Client;
   //$client = new Client();
   $crawler = $client->request('GET', 'https://www.youtube.com/results?search_query=php+web+scraping&sp=CAASBAgEEAE%253D');
   sleep(3);
-  //$d = $c->html();
-  //echo($d);
+  $d = $c->html();
+  echo($d);
   $client->clickLink('php web scraping tutorial(simple)');
   //$crawler = $client->click($link);
   //sleep(15);
