77c2cb111d5b9596e7f8590996b944dc62ea9f2d
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-14 21:39:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 <?php
   ignore_user_abort(true);  
   require 'vendor/autoload.php';
-  $client = \Symfony\Component\Panther\Client::createChromeClient();
-  //use Goutte\Client;
-  //$client = new Client();
+  //$client = \Symfony\Component\Panther\Client::createChromeClient();
+  use Goutte\Client;
+  $client = new Client();
   $client->request('GET', 'https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
   sleep(15);
   //$d = $c->html();
