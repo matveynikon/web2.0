28df1a8c1ebf76c916670db215cf4832b5c912c1
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-15 18:52:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,9 @@
 </head>
 <body>
 <?php
-  ignore_user_abort(true);  
-  require 'vendor/autoload.php';
-  $client = \Symfony\Component\Panther\Client::createChromeClient();
-  //use Goutte\Client;
-  //$client = new Client();
-  $client->request('GET', 'https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
-  sleep(15);
-  //$d = $c->html();
-  echo('hey');
-  //$client->clickLink('php web scraping tutorial(simple)');
-  //$crawler = $client->click($link);
-  //sleep(15);
+exec("php goutte.php",$o,$v);
+print_r($o);
+echo($v);
 ?>
 </body>
 </head>
