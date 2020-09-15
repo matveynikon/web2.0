60c0a4b335c82d46ae1e874629ccab67d9aba586
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-15 19:09:05 +0200

    Update goutte.php

--- goutte.php
+++ goutte.php
@@ -1,9 +1,6 @@
 <?php
-  ignore_user_abort(true);  
   require 'vendor/autoload.php';
   $client = \Symfony\Component\Panther\Client::createChromeClient();
-  //use Goutte\Client;
-  //$client = new Client();
   echo("mnames");
   try{
     $client->request('GET', 'https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
