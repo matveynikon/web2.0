275362c17696cf190e70985a84a6a32e2185c472
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-15 19:54:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,21 +7,10 @@
 <body>
 <?php
   ignore_user_abort(true);  
-  exec("kill $(lsof -t -i:9529)");  
+  exec("kill $(lsof -t -i:9529)",$o,$v);  
   sleep(2);
-  require 'vendor/autoload.php';
-  $client = \Symfony\Component\Panther\Client::createChromeClient();
-  //use Goutte\Client;
-  //$client = new Client();
-  try{
-    $client->request('GET', 'https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
-  }
-  catch(exception $b){
-    echo("jeff");
-  }
-  sleep(15);
-//print_r($o);
-//echo($v);
+  var_dump($o);
+  var_dump($v);
 ?>
 </body>
 </head>
