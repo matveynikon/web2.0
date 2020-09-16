a7e7b2508fba176824ac7fd1e8b8941990e22e7f
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-16 16:03:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,26 @@
 
 </head>
 <body>
-
-</body>
+<?php
+  //ignore_user_abort(true);  
+  //exec("kill $(lsof -t -i:9529)");  
+  /*exec("php goutte.php",$o,$v);  
+  sleep(2);
+  printf($o);
+  printf($v);*/
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);  
+  require 'vendor/autoload.php';
+  echo("mnames");
+  try{
+    //$client = \Symfony\Component\Panther\Client::createChromeClient();
+    //use Goutte\Client;
+    $client = new Goutte\Client();    
+    $client->request('GET', 'https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
+    sleep(15);
+  }
+  catch(exception $b){
+    echo("jeff");
+  }
+?></body>
 </head>
